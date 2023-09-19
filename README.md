41e14fea1655ef9ca0e4e8b4b37971d99bd748a2
Fabio Oliveira <fabiorsao@gmail.com>
2023-09-19 02:32:16 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tab-news
+
 clone development of the tab-news platform for learning and improvement purposes in development
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Confetti from 'react-confetti';
-import useWindowSize from 'react-use/lib/useWindowSize';
+import React, { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 function Home() {
   const containerStyle = {
-    fontSize: '18px',
-    color: 'red',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: '10px',
+    fontSize: "18px",
+    color: "red",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: "10px",
   };
 
   const { width, height } = useWindowSize();
@@ -20,7 +20,7 @@ function Home() {
 
   return (
     <div style={containerStyle}>
-      Oii, Xuu! <span style={{ fontStyle: 'italic' }}>Te amo</span>
+      Oii, Xuu! <span style={{ fontStyle: "italic" }}>Te amo</span>
       {shouldShowConfetti && <Confetti width={width} height={height} />}
     </div>
   );
