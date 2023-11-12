e63618765507f599ac6f395eeea6defc26f8f15e
Fabio Oliveira <fabiorsao@gmail.com>
2023-11-12 22:44:13 +0000

    feat: add files `.env.development`, `database.js`

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,4 +1,8 @@
-export default function status(request, response) {
+import database from "../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT NOW() AS data_e_hora_atual");
+  console.log(result.rows);
   // your code here
   response.status(200).json({ status: "OK" });
 }
